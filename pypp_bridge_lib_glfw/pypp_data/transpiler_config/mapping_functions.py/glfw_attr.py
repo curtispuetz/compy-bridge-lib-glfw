cfa36140cb9825d31b982a923cf23529e62d0f76
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-19 16:32:50 -0700

    move mapping functions to py files

---
+++ pypp_bridge_lib_glfw/pypp_data/transpiler_config/mapping_functions.py/glfw_attr.py
@@ -0,0 +1,8 @@
+import ast
+
+
+def glfw_map_attr(_node: ast.Attribute, _d, res_str: str):
+    attr_str: str = res_str.split(".")[-1]
+    if attr_str.isupper():
+        return f"GLFW_{attr_str}"
+    return res_str
